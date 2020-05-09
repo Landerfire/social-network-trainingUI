7e1e9f3d97bdefb1515100ca367c7928fb6a65bb
Alex Lander <alanderlex@gmail.com>
2020-05-09 13:39:49 +0500

    Updates

---
+++ precache-manifest.851e34c4f99509c1775d7e41efeab858.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6585528a439e4cfbffb8237dd49f36cc",
+    "url": "/IT-KAMA/index.html"
+  },
+  {
+    "revision": "0df7fdeede7ae374b324",
+    "url": "/IT-KAMA/static/css/3.a24f2305.chunk.css"
+  },
+  {
+    "revision": "3cc17e7c059c18e336b4",
+    "url": "/IT-KAMA/static/css/4.fd8676db.chunk.css"
+  },
+  {
+    "revision": "9dba441ffdeb9be0a2cc",
+    "url": "/IT-KAMA/static/css/main.ec6f7117.chunk.css"
+  },
+  {
+    "revision": "4bfa5d0c6a175791e0c8",
+    "url": "/IT-KAMA/static/js/2.31a35c06.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/IT-KAMA/static/js/2.31a35c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df7fdeede7ae374b324",
+    "url": "/IT-KAMA/static/js/3.fc84f4f1.chunk.js"
+  },
+  {
+    "revision": "3cc17e7c059c18e336b4",
+    "url": "/IT-KAMA/static/js/4.04798cfe.chunk.js"
+  },
+  {
+    "revision": "9dba441ffdeb9be0a2cc",
+    "url": "/IT-KAMA/static/js/main.447e1d7c.chunk.js"
+  },
+  {
+    "revision": "f6bff9cee7e3b31ffe7c",
+    "url": "/IT-KAMA/static/js/runtime-main.c225b90b.js"
+  },
+  {
+    "revision": "4ad9c6fe29570efbde0f20a4ceee19ae",
+    "url": "/IT-KAMA/static/media/preloader.4ad9c6fe.gif"
+  },
+  {
+    "revision": "960d9c1c928cebe61055dc3b0bcab8a3",
+    "url": "/IT-KAMA/static/media/user.960d9c1c.png"
+  }
+]);
