a066132c1abb3f5a9b9c129409d7d9eafdc44bee
Alex Lander <alanderlex@gmail.com>
2020-05-17 17:20:02 +0500

    Added icons for Contacts block

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,7 +3,15 @@ import s from './ProfileInfo.module.scss';
 import Preloader from '../../Common/Preloader/Preloader';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 import userPhoto from '../../../assets/images/user.png';
-import ProfileDataForm from "./ProfileDataForm";
+import ProfileDataForm from './ProfileDataForm';
+import vk from '../../../assets/images/vk.svg';
+import facebook from '../../../assets/images/facebook.svg';
+import github from '../../../assets/images/github.svg';
+import instagram from '../../../assets/images/instagram.svg';
+import twitter from '../../../assets/images/twitter.svg';
+import youtube from '../../../assets/images/youtube.svg';
+import website from '../../../assets/images/website.png';
+import mainlink from '../../../assets/images/mainlink.png';
 
 const ProfileInfo = ({isOwner, profile, status, updateStatus, savePhoto, saveProfile}) => {
 
@@ -87,17 +95,63 @@ const ProfileContacts = ({profile}) => {
         <p><b>Contacts</b>:</p>
         <div className={s.contacts}>
             {Object.keys(profile.contacts).map(key => {
-                return <Contact contactId={key} key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+                return <Contact contactId={key} iconName={key} key={key} contactTitle={key} contactValue={profile.contacts[key]} />
             })}
         </div>
     </div>
 }
 
+const iconSort = (iconName) => {
+    const icon = (iconPath, iconName, title) => {
+        return <img className={s.contactIcon} src={iconPath} alt={iconName} title={title} />
+    }
+    switch (iconName) {
+        case "facebook": {
+            let [title, iconPath] = ["facebook.com", facebook];
+            return icon(iconPath, iconName, title);
+        }
+        case "website": {
+            let [title, iconPath] = ["My website", website];
+            return icon(iconPath, iconName, title);
+        }
+        case "vk": {
+            let [title, iconPath] = ["vk.com", vk];
+            return icon(iconPath, iconName, title);
+        }
+        case "twitter": {
+            let [title, iconPath] = ["twitter.com", twitter];
+            return icon(iconPath, iconName, title);
+        }
+        case "instagram": {
+            let [title, iconPath] = ["instagram.com", instagram];
+            return icon(iconPath, iconName, title);
+        }
+        case "youtube": {
+            let [title, iconPath] = ["youtube.com", youtube];
+            return icon(iconPath, iconName, title);
+        }
+        case "github": {
+            let [title, iconPath] = ["github.com", github];
+            return icon(iconPath, iconName, title);
+        }
+        case "mainLink": {
+            let [title, iconPath] = ["Main Link", mainlink];
+            return icon(iconPath, iconName, title);
+        }
+        default: {
+            return undefined
+        }
+    }
+}
 
-const Contact = ({contactTitle, contactValue, contactId}) => {
+
+const Contact = ({contactTitle, contactValue, contactId, iconName}) => {
     return <div>
         {contactValue &&
-        <a id={contactId} href={contactValue} target="_blank" rel="noopener noreferrer">{contactTitle}</a>
+        <a className={s.contact} id={contactId} href={contactValue} target="_blank" rel="noopener noreferrer">
+            {/*{contactTitle}*/}
+            {iconSort(iconName)}
+        </a>
         }
     </div>
 }
